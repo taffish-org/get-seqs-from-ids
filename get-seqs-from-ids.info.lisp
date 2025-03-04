b476308e865bbd843f14885b16992e8a6c3ca32e
Kaiyuan Han <hermit_han@163.com>
2025-03-04 22:59:26 +0800

    Initial commit

---
+++ get-seqs-from-ids.info.lisp
@@ -0,0 +1,7 @@
+((:name           "get-seqs-from-ids")
+ (:repo           "https://github.com/HermitHan/get-seqs-from-ids")
+ (:author         "KaiyuanHan")
+ (:version        "1.0.0")
+ (:dependencies)
+ (:functions      "sbcl"
+                  "[package]hanky-lisp"))
