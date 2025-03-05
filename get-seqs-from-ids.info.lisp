e1c2cfd886bec03c69a5d1fad5163372ac0c006c
Kaiyuan Han <hermit_han@163.com>
2025-03-05 22:00:50 +0800

    Update get-seqs-from-ids.info.lisp

--- get-seqs-from-ids.info.lisp
+++ get-seqs-from-ids.info.lisp
@@ -2,6 +2,7 @@
 (:repo           "HermitHan/get-seqs-from-ids")
 (:author         "KaiyuanHan")
 (:version        "1.0.0")
+(:intro          "Get-Seqs-From-IDs: this app use "common lisp"(need hanky-lisp package) to search ids(from a txt file which have line by line ids) from a fasta file(ids in '> ...' line), and format the ids' fasta seqs(or save to an output file).")
 (:dependencies)
 (:functions      "sbcl"
                  "[package]hanky-lisp")
