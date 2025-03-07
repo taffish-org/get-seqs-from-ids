34dec31f6b8552e50b2105d6caff96dce4a9595f
Kaiyuan Han <hermit_han@163.com>
2025-03-08 01:05:48 +0800

    update taffish-org & registry to ghrc.io

--- get-seqs-from-ids.info.lisp
+++ get-seqs-from-ids.info.lisp
@@ -1,8 +1,8 @@
 (:name           "get-seqs-from-ids")
-(:repo           "HermitHan/get-seqs-from-ids")
+(:repo           "taffish-org/get-seqs-from-ids")
 (:author         "KaiyuanHan")
 (:version        "1.0.0")
-(:intro          "Get-Seqs-From-IDs: this app use "common lisp"(need hanky-lisp package) to search ids(from a txt file which have line by line ids) from a fasta file(ids in '> ...' line), and format the ids' fasta seqs(or save to an output file).")
+(:intro          "Get-Seqs-From-IDs: this app use \"common lisp\"(need hanky-lisp package) to search ids(from a txt file which have line by line ids) from a fasta file(ids in '> ...' line), and format the ids' fasta seqs(or save to an output file).")
 (:dependencies)
 (:functions      "sbcl"
                  "[package]hanky-lisp")
